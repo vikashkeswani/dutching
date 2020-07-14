24997e01a29a078352c50fe4cab0df0a64996405
vikash keswani <vikashkeswani@gmail.com>
2020-07-14 05:37:27 -0700

    updated target wager

--- js/app.js
+++ js/app.js
@@ -76,16 +76,22 @@ function calculateDutching(){
    var profitPercHorse6 = 0 ;
    
 
+   var targetAmount =  JSON.stringify(Number($("#targetAmount").val())) ;
+   if(targetAmount <=0){
+      alert("Enter the Total Wager correctly!") ;
+   }
    
-   if(oddsHorse1 !== 0 && oddsHorse1 > 0) {
+   if((oddsHorse1 !== 0 && oddsHorse1 > 0) && targetAmount > 0) {
+      
+      $("dutchAmount").val(targetAmount) ;
 
       profitPercHorse1 = (1/(oddsHorse1)) * 100 ;
    $("#percentageProfit1").text(profitPercHorse1.toFixed(2)) ;
 
-   var betAmount1 = (profitPercHorse1/(profitPercHorse1 + profitPercHorse2 + profitPercHorse3 + profitPercHorse4)) * 100 ;
+   var betAmount1 = (profitPercHorse1/(profitPercHorse1 + profitPercHorse2 + profitPercHorse3 + profitPercHorse4)) * targetAmount ;
    $("#betAmount1").text(betAmount1.toFixed(2)) ;
 
-   var profitAmount1 = (profitPercHorse1/100) * betAmount1 ;
+   var profitAmount1 = (profitPercHorse1/targetAmount) * betAmount1 ;
    $("#betProfit1").text(profitAmount1.toFixed(2)) ;
 
    
@@ -95,54 +101,50 @@ function calculateDutching(){
   
    }
 
-   if(oddsHorse2 !== 0 && oddsHorse2 > 0) {
+   if((oddsHorse2 !== 0 && oddsHorse2 > 0) && targetAmount > 0) {
    profitPercHorse2 = (1/(oddsHorse2)) * 100 ;
    $("#percentageProfit2").text(profitPercHorse2.toFixed(2)) ;
 
-   var betAmount2 = (profitPercHorse2/(profitPercHorse1 + profitPercHorse2 + profitPercHorse3 + profitPercHorse4)) * 100 ;
+   var betAmount2 = (profitPercHorse2/(profitPercHorse1 + profitPercHorse2 + profitPercHorse3 + profitPercHorse4)) * targetAmount ;
    $("#betAmount2").text(betAmount2.toFixed(2)) ;
 
-   var profitAmount2 = (profitPercHorse2/100) * betAmount1 ;
-   $("#betProfit2").text(profitAmount2.toFixed(2)) ;
+   // var profitAmount2 = (profitPercHorse2/100) * betAmount1 ;
+   // $("#betProfit2").text(profitAmount2.toFixed(2)) ;
 
-   var profitAmount2 = (profitPercHorse2/100) * betAmount2 ;
+   var profitAmount2 = (profitPercHorse2/targetAmount) * betAmount2 ;
    $("#betProfit2").text(profitAmount2.toFixed(2)) ;
 
    
    var amountBack2 = profitAmount2 + betAmount2 ;
    $("#backAmount2").text(amountBack2.toFixed(2)) ;
 
-
-
    
    }
    
-   if(oddsHorse3 !== 0 && oddsHorse3 > 0) {
+   if((oddsHorse3 !== 0 && oddsHorse3 > 0) && targetAmount > 0) {
    profitPercHorse3 = (1/(oddsHorse3)) * 100 ;
    $("#percentageProfit3").text(profitPercHorse3.toFixed(2)) ;
 
-   var betAmount3 = (profitPercHorse3/(profitPercHorse1 + profitPercHorse2 + profitPercHorse3 + profitPercHorse4)) * 100 ;
+   var betAmount3 = (profitPercHorse3/(profitPercHorse1 + profitPercHorse2 + profitPercHorse3 + profitPercHorse4)) * targetAmount ;
    $("#betAmount3").text(betAmount3.toFixed(2)) ;
 
-   var profitAmount3 = (profitPercHorse3/100) * betAmount3 ;
+   var profitAmount3 = (profitPercHorse3/targetAmount) * betAmount3 ;
    $("#betProfit3").text(profitAmount3.toFixed(2)) ;
 
    
    var amountBack3 = profitAmount3 + betAmount3 ;
    $("#backAmount3").text(amountBack3.toFixed(2)) ;
 
-
-
    }
 
-   if(oddsHorse4 !== 0 && oddsHorse4 > 0) {
+   if((oddsHorse4 !== 0 && oddsHorse4 > 0) && targetAmount > 0) {
    profitPercHorse4 = (1/(oddsHorse4)) * 100 ;
    $("#percentageProfit4").text(profitPercHorse4.toFixed(2)) ;
 
-   var betAmount4 = (profitPercHorse4/(profitPercHorse1 + profitPercHorse2 + profitPercHorse3 + profitPercHorse4)) * 100 ;
+   var betAmount4 = (profitPercHorse4/(profitPercHorse1 + profitPercHorse2 + profitPercHorse3 + profitPercHorse4)) * targetAmount ;
    $("#betAmount4").text(betAmount4.toFixed(2)) ;
 
-   var profitAmount4 = (profitPercHorse4/100) * betAmount4 ;
+   var profitAmount4 = (profitPercHorse4/targetAmount) * betAmount4 ;
    $("#betProfit4").text(profitAmount4.toFixed(2)) ;
 
    
@@ -152,14 +154,14 @@ function calculateDutching(){
 
    }
     
-   if(oddsHorse5 !== 0 && oddsHorse5 > 0) {
+   if((oddsHorse5 !== 0 && oddsHorse5 > 0) && targetAmount > 0) {
    profitPercHorse5 = 100 / oddsHorse5 ;
    $("#percentageProfit5").text(profitPercHorse5.toFixed(2)) ;
 
-   var betAmount5 = ( profitPercHorse5 / (profitPercHorse5 + profitPercHorse6)) * 100 ;
+   var betAmount5 = ( profitPercHorse5 / (profitPercHorse5 + profitPercHorse6)) * targetAmount ;
    $("#betAmount5").text(betAmount5.toFixed(2)) ;
 
-   var profitAmount5 = (profitPercHorse5/100) * betAmount5 ;
+   var profitAmount5 = (profitPercHorse5/targetAmount) * betAmount5 ;
    $("#betProfit5").text(profitAmount5.toFixed(2)) ;
 
    
@@ -169,14 +171,15 @@ function calculateDutching(){
 
    }
 
-   if(oddsHorse6 !== 0 && oddsHorse6 > 0) {
+
+   if((oddsHorse6 !== 0 && oddsHorse6 > 0) && targetAmount > 0) {
    profitPercHorse6 = 100 / oddsHorse6 ;
    $("#percentageProfit6").text(profitPercHorse6.toFixed(2)) ;
 
-   var betAmount6 = ( profitPercHorse6 / (profitPercHorse5 + profitPercHorse6)) * 100 ;
+   var betAmount6 = ( profitPercHorse6 / (profitPercHorse5 + profitPercHorse6)) * targetAmount ;
    $("#betAmount6").text(betAmount6.toFixed(2)) ;
 
-   var profitAmount6 = (profitPercHorse6/100) * betAmount6 ;
+   var profitAmount6 = (profitPercHorse6/targetAmount) * betAmount6 ;
    $("#betProfit6").text(profitAmount6.toFixed(2)) ;
 
    
